e0c578771454dad9db322834ad44121377a23bfb
Ryan Tablada <ryan.tablada@gmail.com>
2015-10-04 17:19:26 -0500

    Show clans in sidebar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,27 @@
 */
 
 Route::get('/', function () {
-    $users = App\User::with('clans', 'friendsOut', 'friendsIn')->get();
+    $users = App\User::with('clans', 'friendsOut', 'friendsIn')->paginate(25);
 
-    return view('results', compact('users'));
+    $clansInfo = $users->reduce(function($carry, $user) {
+        $userClans = $user->clans;
+
+        return $userClans->reduce(function($c2, $clan) {
+            if (isset($c2[$clan->tag])) {
+                $x = $c2[$clan->tag];
+                $x['count'] ++;
+
+                $c2[$clan->tag] = $x;
+            } else {
+                $c2[$clan->tag] = [
+                    'value' => $clan,
+                    'count' => 1,
+                ];
+            }
+
+            return $c2;
+        }, $carry);
+    }, new Illuminate\Support\Collection());
+
+    return view('results', compact('users', 'clansInfo'));
 });
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -2,40 +2,49 @@
 
 @section('content')
 <div class="row">
-    <div class="col s12 m3">
-        Clans
-    </div>
+  <div class="col s12 m3">
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>Clans</h4></li>
+      @foreach($clansInfo as $clan)
+        <li class="collection-item">
+          {{$clan['value']->name}} -
+          <span class="grey-text text-lighten-1">{{$clan['value']->tag}}</span>
+          <span class="badge">{{$clan['count']}}</span>
+        </li>
+      @endforeach
+    </ul>
+  </div>
 
-    <div class="col s12 m6">
-        <table>
-            <thead>
-                <tr>
-                    <th>Username</th>
-                    <th>Score</th>
-                    <th>Friends</th>
-                    <th>Clans</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($users as $user)
-                    <tr>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->score}}</td>
-                        <td>{{count($user->friends)}}</td>
-                        <td>{{count($user->clans)}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+  <div class="col s12 m6">
+    <table>
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Score</th>
+          <th>Friends</th>
+          <th>Clans</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+          <tr>
+            <td>{{$user->name}}</td>
+            <td>{{$user->score}}</td>
+            <td>{{count($user->friends)}}</td>
+            <td>{{count($user->clans)}}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 
-    <div class="col s12 m3">
-        <div class="row">
-            Top 10
-        </div>
-        <div class="row">
-            Friends
-        </div>
+  <div class="col s12 m3">
+    <div class="row">
+      Top 10
     </div>
+    <div class="row">
+      Friends
+    </div>
+  </div>
 </div>
 @endsection
