7273e49c7a379ee20a842ca46feb1ce73b947f50
Ryan Tablada <ryan.tablada@gmail.com>
2015-10-04 17:42:58 -0500

    Move clans to partial

---
+++ resources/views/partials/clans-for-users.blade.php
@@ -0,0 +1,10 @@
+<ul class="collection with-header">
+  <li class="collection-header"><h4>Clans</h4></li>
+  @foreach($clansInfo as $clan)
+    <li class="collection-item">
+      {{$clan['value']->name}} -
+      <span class="grey-text text-lighten-1">{{$clan['value']->tag}}</span>
+      <span class="badge">{{$clan['count']}}</span>
+    </li>
+  @endforeach
+</ul>
