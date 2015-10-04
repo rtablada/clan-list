fba976bbcfc7d40c093dfc28ad0483eff33c9953
Ryan Tablada <ryan.tablada@gmail.com>
2015-10-04 17:48:21 -0500

    Use clanManager in partial

--- resources/views/partials/clans-for-users.blade.php
+++ resources/views/partials/clans-for-users.blade.php
@@ -1,3 +1,7 @@
+@inject('clanManager', 'App\Services\ClanManager')
+
+<?php $clansInfo = $clanManager->getClansForUsers($users) ?>
+
 <ul class="collection with-header">
   <li class="collection-header"><h4>Clans</h4></li>
   @foreach($clansInfo as $clan)
