6226c2ded314f7f6cf03a4b3930d59dfd9ec90c0
Ryan Tablada <ryan.tablada@gmail.com>
2015-10-04 19:57:27 -0500

    No ad-hoc logic lines

--- resources/views/partials/clans-for-users.blade.php
+++ resources/views/partials/clans-for-users.blade.php
@@ -1,10 +1,8 @@
 @inject('clanManager', 'App\Services\ClanManager')
 
-<?php $clansInfo = $clanManager->getClansForUsers($users) ?>
-
 <ul class="collection with-header">
   <li class="collection-header"><h4>Clans</h4></li>
-  @foreach($clansInfo as $clan)
+  @foreach($clanManager->getClansForUsers($users) as $clan)
     <li class="collection-item">
       {{$clan['value']->name}} -
       <span class="grey-text text-lighten-1">{{$clan['value']->tag}}</span>
